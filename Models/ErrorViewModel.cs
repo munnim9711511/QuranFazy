45f6bb69cbef23c33130d6c8cbba35eda6ca71df
unknown <hdfclocal@hdfc-m6.hdfc.net>
2020-04-15 06:21:55 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quran.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
